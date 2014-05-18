2ca33a79668de40c585fb32c7c7e75acdc8a9762
Kevin Kirsch <ranlvor@starletp9.de>
2014-05-18 14:07:13 +0200

    Send Deltafeed-Useragent
    Because it's not cool for a webmaster to see many requests without a useragent and this useragent tells the webmaster why all the requests are happening

--- load.php
+++ load.php
@@ -3,6 +3,8 @@ require_once("database.php");
 
 function loadData($feedid) {
   global $db;
-  $data = file_get_contents ($db->getFeedURLByID($feedid));
+  $options  = array('http'  => array('user_agent' => 'Deltafeed (Bot; +https://github.com/keine-ahnung/deltafeed)'));
+  $context  = stream_context_create($options);
+  $data = file_get_contents ($db->getFeedURLByID($feedid), false, $context);
   $blobid = $db->addRequestToDatabase($feedid, $data);
 }
